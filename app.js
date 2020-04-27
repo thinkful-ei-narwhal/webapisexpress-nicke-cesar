02d60c67323f55bc7dc2c8f30a0b237e2cb2594b
nevelson <nevelson92@gmail.com>
2020-04-27 15:35:04 -0400

    prob 2

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const express = require('express');
 const morgan = require('morgan');
@@ -9,7 +9,7 @@ app.use(morgan('dev'));
 app.get('/', (req, res) => {
   res.send('Hello Express!');
 });
-  
+
 app.listen(8000, () => {
   console.log('Express server is listening on port 8000!');
 });
@@ -17,17 +17,32 @@ app.listen(8000, () => {
 app.get('/sum', (req, res) => {
   const a = req.query.a;
   const b = req.query.b;
-    
-  if(!a) {
+
+  if (!a) {
     return res.status(400).send('Please provide a value');
   }
-    
-  if(!b) {
+
+  if (!b) {
     return res.status(400).send('Please provide b value');
   }
 
-  const c = parseInt(a)+parseInt(b);
+  const c = parseInt(a) + parseInt(b);
 
   const sum = `The sum of ${a} and ${b} is ${c}`;
   res.send(sum);
+});
+
+app.get('/cipher', (req, res) => {
+  const text = req.query.text;
+  const shift = req.query.shift;
+
+  let cipher = '';
+  for (let i = 0; i < text.length; i++) {
+    cipher = cipher + text.fromCharCode('A'.charCodeAt(0) + shift);
+  }
+
+
+
+  const cipheredText = `${cipher}`;
+  res.send(cipheredText);
 });
